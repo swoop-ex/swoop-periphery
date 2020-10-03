54257a689eb773a2b77b11ce9250431f6df6af86
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-10-03 17:44:15 +0200

    Update code hash

--- contracts/libraries/UniswapV2Library.sol
+++ contracts/libraries/UniswapV2Library.sol
@@ -22,7 +22,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'87356c32b1d11f0ecc268fbd499639821bf3bcbd0547a703a3437ff4673abb84' // init code hash
             ))));
     }
 
